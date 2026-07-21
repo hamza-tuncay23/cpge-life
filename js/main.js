d459110fdc79d93b395ce34d4c16a57546f3231e
Hamza Margal <hamzamargal2005@gmail.com>
2026-07-22 00:41:27 +0100

    Ajout des fichiers de base (css, js, data, index)

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// ============================================
+// FICHIER : /js/main.js
+// DESCRIPTION : Script principal - À compléter
+// ============================================
+
+console.log("CPGE Life - Chargement réussi !");
